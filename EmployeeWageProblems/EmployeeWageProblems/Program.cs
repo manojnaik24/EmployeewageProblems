d9f0044fba521e4d6b17185d72e89a9f2071c5f9
Manoj naik <chittinaik@gamil.com>
2023-09-04 08:59:43 +0530

    [Manoj] add:Employee Present or Absent

---
+++ EmployeeWageProblems/EmployeeWageProblems/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage ew = new EmployeeWage();
+            ew.Attendance();
+
+            Console.ReadLine();
+        }
+    }
+}
