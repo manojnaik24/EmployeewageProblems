326f7668e507bd7a6e60559a99974e2a2826460d
Manoj naik <chittinaik@gamil.com>
2023-09-04 09:24:15 +0530

    [manoj] add:EmployeeDailyWage

--- EmployeeWageProblems/EmployeeWageProblems/EmployeeWage.cs
+++ EmployeeWageProblems/EmployeeWageProblems/EmployeeWage.cs
@@ -8,18 +8,24 @@ namespace EmployeeWageProblems
 {
     public  class EmployeeWage
     {
+        const int wagePerHour = 20,  isFullTime = 1;
+        int fullDayHour = 0;
         public void Attendance()
         {
             Random random = new Random();
+
             int empCheck = random.Next(0,2);
-            if (empCheck == 0 )
+            if (empCheck == isFullTime )
             {
                 Console.WriteLine("Employee is Present");
+                fullDayHour=8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
+            int empWage = fullDayHour * wagePerHour;
+            Console.WriteLine("Daily Wage is:" + empWage);
         }
     }
 }
